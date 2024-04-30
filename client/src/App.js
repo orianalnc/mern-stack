04acc01eb137640356bfb106d3ce26972c25a2ea
orianalnc <orianalnc94@gmail.com>
2024-04-30 09:02:49 -0400

    add config - init project

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/style.css';
+import UserList from './components/UserList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" bg-slate-700 h-screen w-auto text-zinc-50 ">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold text-center uppercase"> Crud Mern Stack by Orianalnc </h1>
+        <div className="mt-6">
+          <UserList />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/UserList.jsx
@@ -0,0 +1,8 @@
+function UserList(){
+    return(
+        <><h2 className="text-white">UserList</h2></>
+    
+)
+}
+
+export default UserList;
